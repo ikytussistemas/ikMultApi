a550a33a1a864d3b2d39d9ac6232fa7c0403bb24
Francisco José C. Silva <franzecs@gmail.com>
2021-01-06 08:58:36 -0300

    Project created

---
+++ backend/src/test/java/com/ikytus/kdeliver/KdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikytus.kdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
